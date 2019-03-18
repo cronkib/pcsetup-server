b79e29964e3776dff7f5064f9ab3273f6219ab20
Travis Kern <kern.travis@gmail.com>
2019-03-17 23:13:17 -0400

    Add logging to error handler

--- src/main/java/net/astercrono/pcsetup/controller/ErrorHandlerController.java
+++ src/main/java/net/astercrono/pcsetup/controller/ErrorHandlerController.java
@@ -1,5 +1,7 @@
 package net.astercrono.pcsetup.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,14 +10,18 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import net.astercrono.pcsetup.model.PCSResponseModel;
 import net.astercrono.pcsetup.model.PCSResponseStatus;
+import net.astercrono.pcsetup.service.impl.MainAuthenticationService;
 import net.astercrono.pcsetup.validation.ValidationException;
 import net.astercrono.pcsetup.validation.ValidationMessages;
 
 @ControllerAdvice
 public class ErrorHandlerController extends ResponseEntityExceptionHandler {
+	private Logger logger = LoggerFactory.getLogger(MainAuthenticationService.class);
+
 	@ExceptionHandler(ValidationException.class)
 	public ResponseEntity<PCSResponseModel<ValidationMessages>> handleValidationException(
 			ValidationException exception) {
+		logger.debug("Validation Error", exception);
 		return new ResponseEntity<>(
 				new PCSResponseModel<>(exception.getValidationMessages(), PCSResponseStatus.VALIDATION_ERROR),
 				HttpStatus.BAD_REQUEST);
@@ -23,6 +29,7 @@ public class ErrorHandlerController extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<PCSResponseModel<ValidationMessages>> handleException(ValidationException exception) {
+		logger.error("Unexpected Error", exception);
 		return new ResponseEntity<>(new PCSResponseModel<>(new ValidationMessages("An unexpected error occurred"),
 				PCSResponseStatus.INTERNAL_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
